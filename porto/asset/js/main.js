8ba1968906e4f570ec4181b4cc7a61256da95df3
haamid10 <apdixamiid919@gmail.com>
2022-09-17 20:13:21 +0300

    new 5

--- porto/asset/js/main.js
+++ porto/asset/js/main.js
@@ -14,9 +14,10 @@ showMenu('nav-toggle' ,'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
+  // Active link
     navLink.forEach( n => n.classList.remove('active'))
     this.classList.add('active')
-
+// remove menu 
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
 }
