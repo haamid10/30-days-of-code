9b763d717563c82d35464ca20545a41345ef6e7a
haamid10 <apdixamiid919@gmail.com>
2022-09-22 19:16:25 +0300

    begginng of map

--- 01/first.js
+++ 01/first.js
@@ -341,3 +341,8 @@ const wordFrequency= (phrase)=>{
 
 let result = [1,2,3,4,].map(number => number*2)
   console.log(result)
+
+  const doubleNumber = (numbers) => {
+    return numbers.map(number => number * 2)
+  }
+   console.log(doubleNumber([1,2,3,4]))
