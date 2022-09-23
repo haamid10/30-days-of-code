099a7c8968f1d97d0282d2b62b28b9d4af2403a9
haamid10 <apdixamiid919@gmail.com>
2022-09-23 20:45:41 +0300

    begining of DOM manipulation

--- 01/script.js
+++ 01/script.js
@@ -423,5 +423,6 @@ const wordFrequency= (phrase)=>{
       let green = document.getElementById('green')
 
       red.onclick = () => {console.log('red')}
+      // red.innerText = ('djhdh') 
       yellow.onclick = () => {console.log('yellow')}
-      green.onclick = () => {console.log('green')}
+      green.onclick = () => {console.log('green')} 
