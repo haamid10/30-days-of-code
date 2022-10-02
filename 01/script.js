f57800e55de7ed43fab1acace257552120e278f0
haamid10 <apdixamiid919@gmail.com>
2022-10-02 21:11:08 +0300

    main

--- 01/script.js
+++ 01/script.js
@@ -617,6 +617,9 @@ const decreasePeople = () => {
   // guard clause
   // if amount is 1 or less simply return
   // (a.k.a you can't decrease the number of people to 0 or negative!)
+  if (numberOfPeople <= 1){
+    return
+  }
 
   
   // decrement the amount of people
