3739bf7a4227a8bdf25c1556f635dfe42fc7eb90
haamid10 <apdixamiid919@gmail.com>
2022-10-03 19:49:12 +0300

    exercise

--- exercise/script.js
+++ exercise/script.js
@@ -1,1 +1,11 @@
-// alert('wuu buube')
+
+// tipInput
+// numberofpeaple
+// perPersonalTotal
+let billInput = document.getElementById('billInput')
+let  tipInput = document.getElementById(' tipInput')
+let  numberOfPeapleDiv = document.getElementById('numberofpeaple')
+let perPersonalTotalDiv = document.getElementById('perPersonalTotal')
+
+let numberofpeaple = Number(numberOfPeapleDiv.innerText)
+
