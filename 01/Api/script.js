deef5f9a37aec9a2a23dc1a2797477abee43a9bc
haamid10 <apdixamiid919@gmail.com>
2022-10-12 22:10:41 +0300

    fetching  Api

--- 01/Api/script.js
+++ 01/Api/script.js
@@ -3,15 +3,10 @@
 const dogImageDv =document.getElementById('dogImage')
 
 // 
-fetch('https://cat.ceo/api/breeds/image/random')
+fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(json => {
   // console.log(json.message)
  dogImageDv.innerHTML=`<img src='${json.message}' />`
 })
 
-// fetch('https://dog.ceo/api/breeds/image/random')
-// .then(response => response.json())
-// .then(json => {
-//   dogImageDv.innerHTML = `<img src='${json.message}' height=300 width=300/>`
-// })
