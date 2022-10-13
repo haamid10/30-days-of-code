6c857992a02dce0684dab8f3a5aee9d0b6bbae56
haamid10 <apdixamiid919@gmail.com>
2022-10-13 11:33:25 +0300

    fetching  Api super Hero

--- 01/Api/superhero app/script.js
+++ 01/Api/superhero app/script.js
@@ -1,1 +1,8 @@
-const superHero = document.getElementById('imageHero')
+const superHerodiv = document.getElementById('imageHero')
+
+// const superHero = () =>{
+  fetch('https://superheroapi.com/api/access-token/character-id')
+  .then(response => response.json())
+  .then(json =>console.log(json))
+// }
+// console.log(superHero)
