02db8fb58f45a61a078711a801bef8fb81d72bc3
haamid10 <apdixamiid919@gmail.com>
2022-10-13 10:40:41 +0300

    fetching  Api

--- 01/Api/script.js
+++ 01/Api/script.js
@@ -1,12 +1,14 @@
 // https://dog.ceo/api/breeds/image/random
 
 const dogImageDv =document.getElementById('dogImage')
-
+const getDog = document.getElementById('getDog')
 // 
-fetch('https://dog.ceo/api/breeds/image/random')
+const dogimage = () =>
+{fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(json => {
   // console.log(json.message)
- dogImageDv.innerHTML=`<img src='${json.message}' />`
+ dogImageDv.innerHTML=`<img src='${json.message}' height=300px width= 600px  />`
 })
-
+}
+getDog.onclick = () =>dogimage()
