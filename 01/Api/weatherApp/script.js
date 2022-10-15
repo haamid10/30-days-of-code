50de9dc8fb844da70352bb27b831ad763a643969
haamid10 <apdixamiid919@gmail.com>
2022-10-15 19:13:14 +0300

    fetching  Api Asynchronous

--- 01/Api/weatherApp/script.js
+++ 01/Api/weatherApp/script.js
@@ -1,12 +1,12 @@
-const options=  {
+const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '7c73584b6amshddddf219b49d04bp14ffd2jsnde375776e4c8',
-		'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
 	}
 };
 
-fetch('https://openweather43.p.rapidapi.com/weather?q=Chicago&appid=da0f9c8d90bde7e619c3ec47766a42f4&appid=da0f9c8d90bde7e619c3ec47766a42f4&units=standard', options)
+fetch('https://open-weather13.p.rapidapi.com/city/landon', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => console.log(response.main))
 	.catch(err => console.error(err));
