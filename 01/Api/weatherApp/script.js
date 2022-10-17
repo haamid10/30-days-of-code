3bba3fb04f31150b1151aa201e0434e98cae5d64
haamid10 <apdixamiid919@gmail.com>
2022-10-17 19:59:42 +0300

    fetching  Api Asynchronous

--- 01/Api/weatherApp/script.js
+++ 01/Api/weatherApp/script.js
@@ -95,3 +95,31 @@ const fruits =['','']
 const actor = {name: 'hamiid', age: 37, networth: 200000}
  console.log(Object.keys( actor))
  console.log(Object.values( actor))
+
+
+
+
+ class Bank{
+   constructor (balance){
+    this.balance=balance
+   }
+   getBalance (){
+     return balance
+   }
+   deposit (balance){
+    this.balance -= balance
+    console.log('your deposit is ', balance)
+   }
+   withDraw(draw){
+    
+    this.balance += draw 
+    console.log('your withdraw is ', this.balance)
+   }
+ }
+
+ const hamiid = new Bank(100)
+ console.log(hamiid)
+ hamiid.balance
+ hamiid.deposit(20)
+ hamiid.withDraw(100)
+ hamiid.getBalance()
