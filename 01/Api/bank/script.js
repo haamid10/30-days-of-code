1f197c7a17ea4e7c4b4e421c3a903ce15ce12299
haamid10 <apdixamiid919@gmail.com>
2022-10-17 20:14:47 +0300

    fetching  Api Asynchronous

--- 01/Api/bank/script.js
+++ 01/Api/bank/script.js
@@ -15,17 +15,21 @@
    console.log('your deposit is ', balance)
   }
   withDraw(draw){
-   if(this.balance <= 0){
-     console.log
+   if(this.balance - draw <= 0){
+     console.log("you don't have enough money")
    }
    this.balance -= draw 
    console.log('your withdraw is ', draw)
+   console.log('your balance is ', this.balance)
   }
 }
 
-const hamiid = new Bank(100)
-console.log(hamiid)
-hamiid.balance
-hamiid.deposit(20)
-hamiid.withDraw(100)
-hamiid.getBalance()
+const hamiid = new Bank(0)
+
+
+
+// console.log(hamiid)
+// hamiid.balance
+// hamiid.deposit(20)
+// hamiid.withDraw(100)
+// hamiid.getBalance()
