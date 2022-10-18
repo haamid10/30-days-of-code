9b3a0c453db80aa447f15c57b635d7124d1c6b11
haamid10 <apdixamiid919@gmail.com>
2022-10-18 20:34:43 +0300

    fetching  Api Asynchronous

--- 01/Api/netflix clone/netflx.js
+++ 01/Api/netflix clone/netflx.js
@@ -43,10 +43,18 @@ window.onload = () => {
 
 // ** Helper function that makes dynamic API calls **
 function fetchMovies(url, dom_element, path_type) {
+
+  fetch(url)
+  .then((response)=>{
+    console.log(response.json())
+  }).then((data)=>{
+   console.log(data)
+  })
   // Use Fetch with the url passed down 
 
   // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
 }
+fetchMovies('https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213')
 
 //  ** Function that displays the movies to the DOM **
 showMovies = (movies, dom_element, path_type) => {
